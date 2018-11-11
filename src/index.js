494206b9635b571488eb39e8579c4e19cb640ef7
sunnyabc789 <420095734@qq.com>
2018-11-11 15:47:23 +0800

    finish

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import _ from 'lodash'
-import printMe from './print.js';
+// function getComponent() {
+//   return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+//     var element = document.createElement('div');
+    
+//            element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    
+//            return element;
+    
+//          }).catch(error => 'An error occurred while loading the component');
+// }
+//  getComponent().then(component => {
+//      document.body.appendChild(component);
+//    })
 
-function component() {
-  var element = document.createElement('div')
-  var btn = document.createElement('button');
-
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element
+async function getComponent() {
+     var element = document.createElement('div');
+     const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+  
+       element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+       return element;
 }
 
-document.body.appendChild(component())
+getComponent().then(component => {
+  document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,22 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const webpack = require('webpack')
 
 module.exports = {
 	// entry: './src/index.js',
 	entry: {
-		app: './src/index.js',
-		print: './src/print.js'
+		app: './src/index.js'
 	},
 	output: {
 		// filename: 'bundle.js',
 		filename: '[name].bundle.js',
+		chunkFilename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
 			title: 'Output Management'
-		}),
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'common' // 指定公共 bundle 的名称。
 		})
 	],
 	// module: {
