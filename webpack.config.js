62e676a75e2585ac98385512eaf90a9c0a8d8422
sunnyabc789 <420095734@qq.com>
2018-11-11 15:09:59 +0800

    finish-manage-output

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,40 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        //此时用的是use没用inculde exclude
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'url-loader'
-        ]
-      }
-    ]
-  }
-};
+	// entry: './src/index.js',
+	entry: {
+		app: './src/index.js',
+		print: './src/print.js'
+	},
+	output: {
+		// filename: 'bundle.js',
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			title: 'Output Management'
+		})
+	],
+	// module: {
+	//   rules: [
+	//     {
+	//       test: /\.css$/,
+	//       //此时用的是use没用inculde exclude
+	//       use: [
+	//         'style-loader',
+	//         'css-loader'
+	//       ]
+	//     },
+	//     {
+	//       test: /\.(png|svg|jpg|gif)$/,
+	//       use: [
+	//         'url-loader'
+	//       ]
+	//     }
+	//   ]
+	// }
+}
