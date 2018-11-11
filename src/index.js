77b9634b874a0d48b9758f70165ba8c421e1a3da
sunnyabc789 <420095734@qq.com>
2018-11-11 22:10:44 +0800

    webpack3才需要专门配置

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import _ from 'lodash'
-import './style.css'
+import { cube } from './math.js'
 
 function component() {
-  var element = document.createElement('div')
+  var element = document.createElement('pre');
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-  element.classList.add('hello')
-
-  // 将图像添加到我们现有的 div。
+     element.innerHTML =[
+         'Hello webpack!',
+         '5 cubed is equal to ' + cube(5)
+       ].join('\n\n');
 
 
   return element
--- webpack.config.js
+++ webpack.config.js
@@ -6,22 +6,22 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        //此时用的是use没用inculde exclude
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'url-loader'
-        ]
-      }
-    ]
-  }
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/,
+  //       //此时用的是use没用inculde exclude
+  //       use: [
+  //         'style-loader',
+  //         'css-loader'
+  //       ]
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|gif)$/,
+  //       use: [
+  //         'url-loader'
+  //       ]
+  //     }
+  //   ]
+  // }
 };
