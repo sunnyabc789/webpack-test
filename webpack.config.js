af2ab6ccd60569faa05e5d5312bb3eb68538f5f9
sunnyabc789 <420095734@qq.com>
2018-11-11 20:41:56 +0800

    finish

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist'
+	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
