e946280ed862f020bc50b86f30aedd970a037573
sunnyabc789 <420095734@qq.com>
2018-11-11 14:28:18 +0800

    finish

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        //此时用的是use没用inculde exclude
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'url-loader'
+        ]
+      }
+    ]
+  }
+};
