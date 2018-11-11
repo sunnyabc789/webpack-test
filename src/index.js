4d7ac98a65708840a6fa942ebf196dbb36b799a6
sunnyabc789 <420095734@qq.com>
2018-11-11 22:46:19 +0800

    finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import { cube } from './math.js'
 
-function component() {
-  var element = document.createElement('pre');
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!')
+}
 
-     element.innerHTML =[
-         'Hello webpack!',
-         '5 cubed is equal to ' + cube(5)
-       ].join('\n\n');
+function component() {
+  var element = document.createElement('pre')
+
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
 
 
   return element
